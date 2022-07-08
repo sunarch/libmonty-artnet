632e9a712ba5537c5a75e045aea787ebca9cddc5
András Németh <sunarch@protonmail.com>
2022-07-08 18:28:44 +0200

    Add list chunking utility

---
+++ src/libmonty_artnet/utils/grid.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+def chunk_list(source: list,
+               chunk_size: int
+               ) -> list[list]:
+
+    if len(source) > chunk_size:
+        return [source[0:chunk_size]] + \
+               chunk_list(source[chunk_size:], chunk_size=chunk_size)
+    else:
+        return [source]
