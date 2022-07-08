caa075c64038c7d99f54befb79026cb130b63457
András Németh <sunarch@protonmail.com>
2022-07-08 18:18:03 +0200

    Add diagnostics priority codes

---
+++ src/libmonty_artnet/protocol/diag_priority_codes.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Table 5 – Priority Codes
+
+The following table details the Diagnostics Priority codes.
+These are used in ArtPoll and ArtDiagData.
+
+Messages of type 'DpVolatile' are displayed on a single line
+    in the DMX-Workshop diagnostics display.
+All other types are displayed in a list box.
+
+Enum field name: Mnemonic
+Enum field value: Code (hex coded integer)
+"""
+
+from enum import Enum
+
+
+class DiagPriorityCode(Enum):
+
+    # Low priority message.
+    DpLow = 0x10
+
+    # Medium priority message.
+    DpMed = 0x40
+
+    # High priority message.
+    DpHigh = 0x80
+
+    # Critical priority message.
+    DpCritical = 0xe0
+
+    # Volatile message.
+    DpVolatile = 0xf0
